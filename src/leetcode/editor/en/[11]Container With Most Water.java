d27085059fc4f8d8a730508d41b8f1f9c10de910
huangchanghuan <changhuan.huang@gmail.com>
2022-06-23 08:57:58 +0800

    <modify> 修改

---
+++ src/leetcode/editor/en/[11]Container With Most Water.java
@@ -0,0 +1,48 @@
+//You are given an integer array height of length n. There are n vertical lines 
+//drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
+// 
+//
+// Find two lines that together with the x-axis form a container, such that the 
+//container contains the most water. 
+//
+// Return the maximum amount of water a container can store. 
+//
+// Notice that you may not slant the container. 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: height = [1,8,6,2,5,4,8,3,7]
+//Output: 49
+//Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,
+//3,7]. In this case, the max area of water (blue section) the container can conta
+//in is 49.
+// 
+//
+// Example 2: 
+//
+// 
+//Input: height = [1,1]
+//Output: 1
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// n == height.length 
+// 2 <= n <= 105 
+// 0 <= height[i] <= 104 
+// 
+// Related Topics Array Two Pointers Greedy 
+// 👍 17619 👎 969
+
+package leetcode.editor.en;
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int maxArea(int[] height) {
+        return 1;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
